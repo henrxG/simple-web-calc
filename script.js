842b17688111f123b797a65390347590bc625d2d
henry <89320103+henrxG@users.noreply.github.com>
2024-06-11 01:33:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let a = 0;
+let b = 0;
+let operator = "";
+
+
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
